997ff1942b0cbc4a9af93716c107d10a1c292a60
rm1377 <52181737+rm1377@users.noreply.github.com>
2022-11-22 19:57:43 +0330

    working with api view not complited

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from profiles_api import serializers
+
+
+class HelloApiView(APIView):
+    """test apiview"""
+    serializers_class = serializers.HelloSerializer
+
+    def get(self, request, format=None, *args, **kwargs):
+        return Response({'message': 'Hello!'})
+
+    def post(self, request):
+        """create a hello message with our name"""
+        serializer = self.serializers_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST,
+            )
